5101a5d023207191b75fa0c198e4e6d8c5e9f32e
rtcody <166568786+rtcody@users.noreply.github.com>
2024-04-17 19:40:57 -0700

    Update deck.hpp

--- deck.hpp
+++ deck.hpp
@@ -1,75 +1,20 @@
+#pragma once
 #include "card.hpp"
-#define deckSize 52
 
-class Deck
+#define deckSize 52
+ 
+class Deck 
 {
 public:
-	Deck(); 
-	~Deck();  
+	Deck();
+	~Deck();
 
-	card* getCards(void);      
+	card* getCards(void);
 
-	card getCard(int& index); 
-	card getCard(int index); 
+	card getCard(int index);
 
-	void shuffle(void); 
+	void shuffle(void);
 
 private:
-	card* cards;   
+	card cards[deckSize]; 
 };
-
-Deck::Deck()
-{
-	card temp; 
-	cards = new card[deckSize];  
-	ifstream file;  
-	file.open("deck.csv", ios::in);
-	string trash;  
-	getline(file, trash);    
-
-	for (int i = 0; i < 52; i++) 
-	{
-		getline(file, trash, ','); 
-		temp.setValue(stoi(trash)); 
-		
-		getline(file, trash, ','); 
-		temp.setSuit(trash[0]); 
-
-		getline(file, trash); 
-		temp.setImage(trash);   
-	}
-}
-
-Deck::~Deck()
-{
-	delete cards;       
-}
-
-card* Deck::getCards()
-{
-	return cards; 
-}
-
-card Deck::getCard(int& index)
-{
-	index++; 
-	return cards[index - 2]; 
-}
-
-card Deck::getCard(int index)
-{
-	return cards[index];         
-}
-
-void Deck::shuffle(void)     
-{
-	int j; 
-	card temp; 
-	for (int i = deckSize - 1; i > 0; i--)
-	{
-		j = rand() % (i + 1);  
-		temp = cards[j];  
-		cards[j] = cards[i];  
-		cards[i] = temp;  
-	} 
-} 
