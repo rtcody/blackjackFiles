5d5334ac755a203e9758a09cefb1c1dae6369cf0
rtcody <166568786+rtcody@users.noreply.github.com>
2024-04-17 19:39:50 -0700

    Update card.hpp - split into cpp file

--- card.hpp
+++ card.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -7,7 +9,13 @@
 #include <ctime>
 #include <cassert>
 
-using namespace std; 
+
+using std::string;
+using std::vector; 
+using std::cout; 
+using std::endl; 
+using std::ifstream; 
+using std::ios; 
 
 class card
 {
@@ -24,65 +32,14 @@ public:
 	void setSuit(char suit); 
 	void setImage(string image); 
 
+
 	void print(void); 
+
+	
+
 private:
 	int value; 
 	char suit; 
 	string image;      
+	
 };
-
-card::card()
-{
-	this->value = 0; 
-	this->suit = '\0'; 
-	this->image = "\0"; 
-}
-
-card::card(int value, char suit, string image)
-{
-	this->value = value; 
-	this->suit = suit; 
-	this->image = image;    
-}
-
-card::card(card& copy)
-{
-	this->value = copy.value; 
-	this->suit = copy.suit; 
-	this->image = copy.image;     
-}
-
-int card::getValue()
-{
-	return value; 
-}
-
-char card::getSuit()
-{
-	return suit; 
-}
-
-string card::getImage()
-{
-	return image; 
-}
-
-void card::setValue(int value)
-{
-	this->value = value; 
-}
-
-void card::setSuit(char suit)
-{
-	this->suit = suit; 
-}
-
-void card::setImage(string image)
-{
-	this->image = image;   
-} 
-
-void card::print()
-{
-	cout << "Value: " << value << ", Suit: " << suit << endl;    
-}
