d92d606eb3008d065349a57ff591738c12b7fe89
rtcody <166568786+rtcody@users.noreply.github.com>
2024-04-17 19:41:48 -0700

    Update hand.hpp

--- hand.hpp
+++ hand.hpp
@@ -1,14 +1,41 @@
 #pragma once
 
-#include "deck.hpp"
+#include "deck.hpp" 
 
+//Joey working on hand 
 class Hand 
 {
 
 public: 
+	/// <summary>
+	/// Hand constructor that takes in game deck 
+	/// </summary>
+	/// <param name="gameDeck"></param>
+	Hand(Deck &gameDeck); 
+	//virtual void displayHand() = 0; // will have a different display hand in both the player and dealer class
+	/// <summary>
+	/// computes the numerical value of the entire hand
+	/// </summary>
+	/// <returns>the total value of the hand</returns>
+	void computeHandValue(); 
+	int computeHandValue(card dummyHand[11]); 
+	/// <summary>
+	/// adds a card to the blackjackHand
+	/// </summary>
+	/// <param name="gameDeck"></param>
+	void hit(Deck &gameDeck);
+	/// <summary>
+	/// virtual display hand (different for both player and dealer 
+	/// </summary>
+	//virtual void displayHand() = 0;  i
+
+	card getCard(int index); 
+	int getHandValue(); 
 	
 
 private: 
-	std::vector<card> blackjackHand; 
+	card blackjackHand[11]; // 11 is the most cards you can have (All Aces) 
+	int cardCount; // used to ensure we do not go out of bounds in the array; 
+	int handValue; // total value of the hand 
 	
 };
