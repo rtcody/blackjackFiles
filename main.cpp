15f274ee3bf053d5128d6f6819087f434b845243
Ho-Quan <166866483+Ho-Quan@users.noreply.github.com>
2024-04-19 14:43:52 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,41 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include "hand.hpp"
+#include "Player.hpp"
+#include "Dealer.hpp"
+#include "test.hpp"
+#include "Button.hpp"
+#include "Client.h"
+
 using sf::Texture;
 using sf::Sprite;
 
-
-int main()
+int main(void)
 {
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "BLACKJACK");
+    //WSADATA wsaData;
+    //SOCKET clientSocket;
+    //sockaddr_in serverAddr;
 
+    //initCreateConnect(wsaData, clientSocket, serverAddr);
+
+    //string message;
+
+    //// Prompt user for input
+    //message = promptForInput();
+
+    //// Send user input to server
+    //sendToServer(message, clientSocket);
+
+    //// Close socket
+    //closeSocket(clientSocket);
+
+    srand((unsigned int)time(NULL));
+
+     Test t;
+     t.runTests();
+
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "BLACKJACK"); 
+     
     Texture b;
     b.loadFromFile("Cards/background.png");
 
@@ -15,35 +43,66 @@ int main()
     background.setScale(1.5f, 1.5f);
 
     Texture x1;
-    x1.loadFromFile("Cards/jack_of_hearts2.png");
-    Texture x2;
-    x2.loadFromFile("Cards/ace_of_diamonds.png");
+    x1.loadFromFile("Cards/card back black.png");
 
-    Sprite card1(x1);
-    card1.setScale(0.35f, 0.35f);
-    card1.setPosition(820, 800);
+    //this is a set position for the backcard and I think we should keep it in main in the future because we can control when we want to remove it
+    Sprite backCard(x1);
+    backCard.setScale(0.26f, 0.26f);
+    backCard.setPosition(950, 50);
 
-    Sprite card2(x2);
-    card2.setScale(0.35f, 0.35f);
-    card2.setPosition(920,700);
+    Deck gDeck;
+    Player player(gDeck);
+    Dealer dealer(gDeck); 
 
+    Texture HitTexture;
+    HitTexture.loadFromFile("Cards/redHitButton.png");
+    HitButton Hit(HitTexture);
 
+    bool playerHit = false;
+
+ 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
+            else if (event.type == sf::Event::MouseButtonPressed) //check for mouse button press
+            {
+                if (event.mouseButton.button == sf::Mouse::Left) //check left mouse button is pressed
+                {
+                    sf::Vector2i mousePosition = sf::Mouse::getPosition(window); 
+                    if (Hit.getGlobalBounds().contains(mousePosition.x, mousePosition.y) && player.getCard(10).getImage() == "\0") //if mouse is clicked the hit buttton
+                    {
+                        playerHit = true;
+                        player.hit(gDeck);  
+                    }
+                }
+            }
+
         }
+
+        if (player.canSplit())
+        {
+            //button for if the player chose to split
+
+
+        }
+
 
 
         window.clear();
         window.draw(background);
-        window.draw(card1);
-        window.draw(card2);
+        window.draw(Hit); 
+        dealer.displayHand(window); 
+        window.draw(backCard); 
+        player.displayHand(window); 
         window.display();
+   
     }
-
     return 0;
 }
+
