727c59aa4dda3ea2980318a8064612d819510884
Jose-Hurtado-Sanchez <146782122+Jose-Hurtado-Sanchez@users.noreply.github.com>
2024-04-19 19:02:11 -0700

    Update Dealer.cpp

--- Dealer.cpp
+++ Dealer.cpp
@@ -1,27 +1,27 @@
 #include "Dealer.hpp"
 
-void Dealer::displayHand(sf::RenderWindow& window) 
+
+void Dealer::displayHand(sf::RenderWindow& window, int x, int y) 
 
 {
-    float x = 950; //starting x coordinate
-    float y = 50; //starting y coordate
-
+   
     for (int i = 0; i < cardCount; i++)
     {
         //get the texture for the current card
         std::string cardImage = blackjackHand[i].getImage();
-        sf::Texture texture;
-        texture.loadFromFile(cardImage);
+        //  sf::Texture texture;
+        blackjackHand[i].setTexture(cardImage);
 
         //create a sprite for the current card
-        sf::Sprite cardSprite(texture);
-        cardSprite.setScale(0.35f, 0.35f);
+     //   sf::Sprite cardSprite(texture);
+        blackjackHand[i].setSprite(blackjackHand[i].getTexture());
+        blackjackHand[i].getSprite().setScale(0.35f, 0.35f);
 
         //set position the card
-        cardSprite.setPosition(x, y);
+        blackjackHand[i].getSprite().setPosition(x, y);
 
         //draw the card to the window
-        window.draw(cardSprite);
+        window.draw(blackjackHand[i].getSprite());
 
         x -= 200; //reposition x coordinate for the next card
     }
