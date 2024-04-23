c25e757ba8191fececbf75e01383ff4fd1833d10
rtcody <166568786+rtcody@users.noreply.github.com>
2024-04-23 12:57:54 -0700

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,94 +1,93 @@
 #include "Server.h"
 
-// Initialize Winsock
-void initializeSocket(WSADATA& wsaData)
+void initializeWSA(WSADATA& wsaData)
 {
     int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0) {
-        std::cerr << "WSAStartup failed: " << iResult << std::endl;
+        std::cerr << "WSAStartup failed." << std::endl;
     }
 }
 
-// Create socket
 void createSocket(SOCKET& serverSocket)
 {
     serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (serverSocket == INVALID_SOCKET) {
-        std::cerr << "Error: Couldn't create socket\n";
+        std::cerr << "Error: Couldn't create socket" << std::endl;
         WSACleanup();
     }
 }
 
-// Bind socket to IP and port
-void bindSocket(sockaddr_in& serverAddr, SOCKET& serverSocket)
+void bindIPAddr(sockaddr_in& serverAddr, SOCKET& serverSocket)
 {
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY; // Listen on any interface
-    serverAddr.sin_port = htons(8080); // Listen on port 8080
+    serverAddr.sin_addr.s_addr = INADDR_ANY; // Listen on any interface 
+    serverAddr.sin_port = htons(8080); // Listen on port 8080 
     if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        std::cerr << "Error: Couldn't bind socket\n";
+        std::cerr << "Error: Couldn't bind socket" << std::endl;
         closesocket(serverSocket);
         WSACleanup();
     }
 }
 
-// Listen for incoming connections
 void listen(SOCKET& serverSocket)
 {
     if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
-        std::cerr << "Error: Couldn't listen on socket\n";
+        std::cerr << "Error: Couldn't listen on socket" << std::endl;
         closesocket(serverSocket);
         WSACleanup();
     }
 
-    std::cout << "Server listening on port 8080...\n";  
+    std::cout << "Server listening on port 8080..." << std::endl;
 }
 
-// Accept incoming connection
-SOCKET acceptConnection(SOCKET& serverSocket) 
+SOCKET acceptCommunications(SOCKET& serverSocket)
 {
     SOCKET clientSocket = accept(serverSocket, NULL, NULL);
     if (clientSocket == INVALID_SOCKET) {
-        std::cerr << "Error: Couldn't accept incoming connection\n";
+        std::cerr << "Error: Couldn't accept incoming connection" << std::endl;
         closesocket(serverSocket);
         WSACleanup();
     }
-    
-    std::cout << "Client connected\n"; 
 
-    return clientSocket;   
-} 
+    std::cout << "Client connected\n";
 
-// Receive data from client until the client stops sending
-void recieveData(SOCKET& clientSocket, SOCKET& serverSocket)
+    return clientSocket;
+}
+
+string recieveData(SOCKET& serverSocket, SOCKET& clientSocket)
 {
     char buffer[1024];
+    string message;
     int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
     if (bytesReceived == 0) {
         // Client has closed the connection
-        std::cout << "Client disconnected\n";
+        std::cout << "Client disconnected" << std::endl;
     }
     else if (bytesReceived == SOCKET_ERROR) {
         std::cerr << "Error: Couldn't receive data from client\n";
         closesocket(clientSocket);
-        closesocket(serverSocket); 
+        closesocket(serverSocket);
         WSACleanup();
-
     }
     else {
-        // Process received data (in this example, just print it)
-        buffer[bytesReceived] = '\0'; // Null-terminate the received data 
-        std::cout << "Received data from client: " << buffer << std::endl;
+        buffer[bytesReceived] = '\0'; // Null-terminate the received data
+        message = buffer;
     }
 
-
-    // Close client socket
-    closesocket(clientSocket);
+    return message;
 }
 
-void closeServer(SOCKET& serverSocket)
+
+void closeSocket(SOCKET& serverSocket, SOCKET& clientSocket)
 {
+    closesocket(clientSocket);
+
     // Close server socket
     closesocket(serverSocket);
     WSACleanup();
+}
+
+void writeToFile(std::ofstream& file, string message)
+{
+    file << message;
 }
